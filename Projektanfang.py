61e1368b8be5d9ed2c262d0a4ec482c03c94795c
Aradmosco <aradmoskovitz@gmail.com>
2025-02-19 18:41:18 +0100

    Add files via upload

--- Projektanfang.py
+++ Projektanfang.py
@@ -58,6 +58,7 @@ class GUI():
         self.tictactoe = Tk()
         self.tictactoe.attributes("-fullscreen", True)  
         self.tictactoe.configure(bg="khaki")
+        self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         
         self.photo_tictactoefeld = Image.open("feld.png")
         self.new_photofeld = self.photo_tictactoefeld.resize((int(self.screen_width /2), int(self.screen_width/2)))
@@ -104,6 +105,13 @@ class GUI():
         self.tugofwar.attributes("-fullscreen", True)  
         self.tugofwar.configure(bg="khaki")
 
+    #def gewonnen(self, gewinner):
+
+    #def check_winner(self):
+  #      if self.board[0] == "X" and self.board[1] == "X" and self.board[2] == "X":
+   #         self.gewonnen("X")
+
+
     def place(self, event=None):
         self.xkor = event.x
         self.ykor = event.y
@@ -112,75 +120,94 @@ class GUI():
             if self.schongewesen1 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o1.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /1.5)
+                    self.board[0] = "O"
                 else:
-                    self.Label_x1.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /1.5)  
+                    self.Label_x1.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /1.5)
+                    self.board[0] = "X"  
                 self.dranncounter += 1
                 self.schongewesen1 +=1
         elif self.xkor < self.screen_width /6 and self.ykor > self.screen_height/3 and self.ykor < self.screen_height/1.6666:
             if self.schongewesen2 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o2.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /0.31)
+                    self.board[1] = "O"
                 else:
                     self.Label_x2.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /0.31)
+                    self.board[1] = "X"
                 self.dranncounter += 1
                 self.schongewesen2 +=1
         elif self.xkor < self.screen_width /6 and self.ykor > self.screen_height/1.6666:
             if self.schongewesen3 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o3.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /0.175)
+                    self.board[2] = "O"
                 else:
                     self.Label_x3.place(x=(self.screen_width /2 -self.screen_width /4.2), y=(self.screen_height-(self.screen_width /2)) /0.175)
+                    self.board[2] = "X"
                 self.dranncounter += 1
                 self.schongewesen3 +=1
         elif self.xkor < self.screen_width /3 and self.ykor < self.screen_height/3:
             if self.schongewesen4 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o4.place(x=(self.screen_width /2 -self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /1.5)
+                    self.board[3] = "O"
                 else:
                     self.Label_x4.place(x=(self.screen_width /2 -self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /1.5)
+                    self.board[3] = "X"
                 self.dranncounter += 1
                 self.schongewesen4 +=1
         elif self.xkor < self.screen_width /3 and self.ykor > self.screen_height/3 and self.ykor < self.screen_height/1.6666:
             if self.schongewesen5 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o5.place(x=(self.screen_width /2 -self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.31)
+                    self.board[4] = "O"
                 else:
                     self.Label_x5.place(x=(self.screen_width /2 -self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.31)
+                    self.board[4] = "X"
                 self.dranncounter += 1
                 self.schongewesen5 +=1
         elif self.xkor < self.screen_width /3 and self.ykor > self.screen_height/1.6666:
             if self.schongewesen6 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o6.place(x=(self.screen_width /2 -self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.175)
+                    self.board[5] = "O"
                 else:
                     self.Label_x6.place(x=(self.screen_width /2 -self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.175)
+                    self.board[5] = "X"
                 self.dranncounter += 1
                 self.schongewesen6 +=1
         elif self.xkor > self.screen_width /3 and self.ykor < self.screen_height/3:
             if self.schongewesen7 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o7.place(x=(self.screen_width /2 +self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /1.5)
+                    self.board[6] = "O"
                 else:
                     self.Label_x7.place(x=(self.screen_width /2 +self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /1.5)
+                    self.board[6] = "X"
                 self.dranncounter += 1
                 self.schongewesen7 +=1
         elif self.xkor > self.screen_width /3 and self.ykor > self.screen_height/3 and self.ykor < self.screen_height/1.6666:
             if self.schongewesen8 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o8.place(x=(self.screen_width /2 +self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.31)
+                    self.board[7] = "O"
                 else:
                     self.Label_x8.place(x=(self.screen_width /2 +self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.31)
+                    self.board[7] = "X"
                 self.dranncounter += 1
                 self.schongewesen8 +=1
         elif self.xkor > self.screen_width /3 and self.ykor > self.screen_height/1.6666:
             if self.schongewesen9 == 0:
                 if self.dranncounter%2 == 0:
                     self.Label_o9.place(x=(self.screen_width /2 +self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.175)
+                    self.board[8] = "O"
                 else:
                     self.Label_x9.place(x=(self.screen_width /2 +self.screen_width /12.5), y=(self.screen_height-(self.screen_width /2)) /0.175)
+                    self.board[8] = "X"
                 self.dranncounter += 1
                 self.schongewesen9 +=1
-            
+        print(self.board)
+        self.check_winner()
 
 if __name__ == "__main__":
     GUI()
