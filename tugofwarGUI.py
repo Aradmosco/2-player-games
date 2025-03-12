583d5566d8020fb584b36f7ec5319124b8bb96e8
Aradmosco <aradmoskovitz@gmail.com>
2025-03-12 15:24:15 +0100

    Update tugofwarGUI.py

--- tugofwarGUI.py
+++ tugofwarGUI.py
@@ -3,39 +3,39 @@ from PIL import Image, ImageTk
 
 class TugOfWarGUI:
     def __init__(self):
-        self.fenster = Tk()
-        self.fenster.attributes("-fullscreen", True) 
-        self.fenster.configure(bg="khaki")
-        self.screen_width = self.fenster.winfo_screenwidth()
-        self.screen_height = self.fenster.winfo_screenheight()
-        
+        self.tugofwarfenster = Tk()
+        self.tugofwarfenster.attributes("-fullscreen", True)
+        self.tugofwarfenster.configure(bg="khaki")
+        self.screen_width = self.tugofwarfenster.winfo_screenwidth()
+        self.screen_height = self.tugofwarfenster.winfo_screenheight()
+       
         self.position =  self.screen_width/4
         self.left_boundary = self.screen_width/3 - self.screen_width/4
         self.right_boundary = self.screen_width/1.5 - self.screen_width/4
         self.step_size = self.screen_width/100
-        
+       
 
-        self.canvas = Canvas(self.fenster, width=self.screen_width, height=self.screen_height, bg="khaki", bd=0, highlightthickness=0)
+        self.canvas = Canvas(self.tugofwarfenster, width=self.screen_width, height=self.screen_height, bg="khaki", bd=0, highlightthickness=0)
         self.canvas.pack()
         self.canvas.create_line(self.screen_width // 3, 0, self.screen_width // 3, self.screen_height, fill="black", width=3)
         self.canvas.create_line(self.screen_width * 2 // 3, 0, self.screen_width * 2 // 3, self.screen_height, fill="black", width=3)
-        
+       
         self.image_tugofwar = Image.open("tugofwar.png")
         self.image_tugofwar_resized = self.image_tugofwar.resize((self.screen_width // 2, self.screen_height // 2))  # Resize the image to exactly half the screen size
         self.photo_tugofwar = ImageTk.PhotoImage(self.image_tugofwar_resized)
-        self.Label_tugofwar = Label(self.fenster, image=self.photo_tugofwar)
+        self.Label_tugofwar = Label(self.tugofwarfenster, image=self.photo_tugofwar)
 
         self.Label_tugofwar.place(x=self.position, y=170)
 
-        
+       
         # Key bindings
-        self.fenster.bind("a", self.move_left)
-        self.fenster.bind("l", self.move_right)
+        self.tugofwarfenster.bind("a", self.move_left)
+        self.tugofwarfenster.bind("l", self.move_right)
 
         # Instructions
-        self.info_label = Label(self.fenster, text="Drücken Sie 'A' (links) vs 'L'(rechts)", font=("Arial", 14), bg="lightblue")
+        self.info_label = Label(self.tugofwarfenster, text="Drücken Sie 'A' (links) vs 'L'(rechts)", font=("Arial", 14), bg="lightblue")
         self.info_label.place(x=0,y=0)
-        self.fenster.mainloop()
+        self.tugofwarfenster.mainloop()
 
     def move_left(self, event):
         if self.position > self.left_boundary:
@@ -52,7 +52,7 @@ class TugOfWarGUI:
             self.gewonnen("blau")
 
     def gewonnen(self, gewinner):
-        winner_window = Toplevel(self.fenster)  # Erstellt ein neues Fenster für den Gewinner
+        winner_window = Toplevel(self.tugofwarfenster)  # Erstellt ein neues Fenster für den Gewinner
         winner_window.attributes("-fullscreen", True)
         winner_window.configure(bg="khaki")
         winner_message = f"{gewinner} gewinnt!"  # Zeigt den Gewinner an
@@ -62,11 +62,8 @@ class TugOfWarGUI:
     # Setzt das Spiel zurück und kehrt zum Hauptbildschirm zurück
     def return_to_main(self, winner_window):
         winner_window.destroy()  # Schließt das Gewinner-Fenster
-        self.fenster.destroy()  # Schließt das Fenster
-        
-
+        self.tugofwarfenster.destroy()  # Schließt das Fenster
+       
 
 if __name__ == "__main__":
     TugOfWarGUI()
-    
-
